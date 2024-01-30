1d1c7175eb3fb4aec258aeb9579064f5477681cf
Your Name <you@example.com>
2024-01-30 12:58:40 +0000

    feat: [自动化录入]舒龙山关于批邓问题的材料附件（1976106+）

---
+++ 62f86815-8b84-48e5-819d-3a5560837cbe.ts
@@ -0,0 +1,47 @@
+export default {
+  resource_type: book,
+  entity: {
+    id: '62f86815-8b84-48e5-819d-3a5560837cbe',
+    name: '舒龙山关于批邓问题的材料附件（1976.10.6+）',
+    internal: true,
+    official: true,
+    type: 'img',
+    author: '七机部二院材料 722',
+    files: ["https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/0.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/1.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/2.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/3.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/4.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/5.jpg"]
+  },
+  {
+  "parser_option": {
+    "articles": [
+      {
+        "title": "舒龙山关于批邓问题的材料附件",
+        "authors": [
+          "七机部"
+        ],
+        "page_start": 1,
+        "page_end": 6,
+        "dates": [
+          {
+            "year": 1977
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "auto_vsplit": true,
+      "vsplit": 0.5,
+      "content_thresholds": [
+        0,
+        0,
+        0,
+        0
+      ],
+      "line_merge_threshold": 30,
+      "standard_paragraph_merge_strategy_threshold": 0,
+      "differential_paragraph_merge_strategy_threshold": 30
+    },
+    "ocr_exceptions": {}
+  }
+},
+  parser_id: 'automation',
+  path: '62f86815-8b84-48e5-819d-3a5560837cbe'
+};
