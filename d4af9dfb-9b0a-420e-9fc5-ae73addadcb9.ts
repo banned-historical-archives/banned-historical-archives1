57b6cebb7c2387c742f8bcd6c1a837c986d60249
Your Name <you@example.com>
2024-02-02 08:23:07 +0000

    feat: [自动化录入]夺取揭批“四人帮”斗争的全胜（197811）

---
+++ d4af9dfb-9b0a-420e-9fc5-ae73addadcb9.ts
@@ -0,0 +1,161 @@
+export default {
+  resource_type: 'book',
+  entity: {
+    id: 'd4af9dfb-9b0a-420e-9fc5-ae73addadcb9',
+    name: '夺取揭批“四人帮”斗争的全胜（1978.11）',
+    internal: false,
+    official: false,
+    type: 'img',
+    author: '中共上海市委揭批“四人帮”运动办公室印，1978.11',
+    files: ["https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/1.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/2.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/3.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/4.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/5.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/6.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/7.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/8.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/9.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/10.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/11.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/12.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/13.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/14.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/15.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/16.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/17.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/18.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/19.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/20.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/21.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/22.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/23.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/24.jpg","https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives1/main/25.jpg"]
+  },
+  parser_option: {
+  "articles": [
+    {
+      "title": "夺取揭批“四人帮”斗争的全胜",
+      "authors": [
+        "中共上海市委揭批“四人帮”运动办公室"
+      ],
+      "page_start": 3,
+      "page_end": 4,
+      "dates": [
+        {
+          "year": 1978,
+          "month": 11
+        }
+      ]
+    },
+    {
+      "title": "继续抓紧抓好清查工作的若干问题",
+      "authors": [
+        "《运动情况》"
+      ],
+      "page_start": 5,
+      "page_end": 6,
+      "dates": [
+        {
+          "year": 1978,
+          "month": 11
+        }
+      ]
+    },
+    {
+      "title": "关于审查对象定性处理的审批手续等问题的意见",
+      "authors": [
+        "《运动情况》"
+      ],
+      "page_start": 6,
+      "page_end": 7,
+      "dates": [
+        {
+          "year": 1978,
+          "month": 11
+        }
+      ]
+    },
+    {
+      "title": "市委召开负责干部会议学习和贯彻党中央四十八号文件",
+      "authors": [
+        "《运动情况》"
+      ],
+      "page_start": 7,
+      "page_end": 9,
+      "dates": [
+        {
+          "year": 1978,
+          "month": 11
+        }
+      ]
+    },
+    {
+      "title": "关于对与“四人帮”有牵连的干部采取临时组织措施的若干规定（口头传达稿）",
+      "authors": [
+        "中共上海市委揭批“四人帮”运动办公室"
+      ],
+      "page_start": 9,
+      "page_end": 10,
+      "dates": [
+        {
+          "year": 1978,
+          "month": 11
+        }
+      ]
+    },
+    {
+      "title": "关于上海市清查工作的情况报告（节录）",
+      "authors": [
+        "中共上海市委揭批“四人帮”运动办公室"
+      ],
+      "page_start": 11,
+      "page_end": 13,
+      "dates": [
+        {
+          "year": 1978,
+          "month": 11
+        }
+      ]
+    },
+    {
+      "title": "严佑民同志在区县局干部会议上讲话",
+      "authors": [
+        "严佑民"
+      ],
+      "page_start": 14,
+      "page_end": 19,
+      "dates": [
+        {
+          "year": 1978,
+          "month": 4,
+          "day": 16
+        }
+      ]
+    },
+    {
+      "title": "关于《清查工作中应注意掌握的几个问题》向市委的请示报告",
+      "authors": [
+        "中共上海市委揭批“四人帮”运动办公室"
+      ],
+      "page_start": 19,
+      "page_end": 19,
+      "dates": [
+        {
+          "year": 1977,
+          "month": 12,
+          "day": 11
+        }
+      ]
+    },
+    {
+      "title": "清查工作中应注意掌握的几个问题",
+      "authors": [
+        "中共上海市委揭批“四人帮”运动办公室"
+      ],
+      "page_start": 20,
+      "page_end": 25,
+      "dates": [
+        {
+          "year": 1977,
+          "month": 12,
+          "day": 11
+        }
+      ]
+    }
+  ],
+  "ocr": {
+    "auto_vsplit": true,
+    "vsplit": 0.5,
+    "content_thresholds": [
+      0,
+      0,
+      0,
+      0
+    ],
+    "line_merge_threshold": 30,
+    "standard_paragraph_merge_strategy_threshold": 0,
+    "differential_paragraph_merge_strategy_threshold": 30
+  },
+  "ocr_exceptions": {}
+},
+  parser_id: 'automation',
+  path: 'd4af9dfb-9b0a-420e-9fc5-ae73addadcb9'
+};
